afc65fe0c0d330d6406c53955a65a81a866ae4f6
Swati Namdev <chirag awasthi>
2023-05-21 11:34:46 +0530

    t

--- src/utils/ApiRequest.js
+++ src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-const host = process.env.BACKEND_URL;
+const host = "stocksensebackend-production.up.railway.app";
 
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
